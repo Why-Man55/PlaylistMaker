c25f29973833492cc7e511bd28699761f8b49e7a
Why-Man55 <Why2Man4@gmail.com>
2024-01-04 21:04:28 +0300

    отправка 1

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_menu)
+
+        val searchButton = findViewById<Button>(R.id.Poisk_but)
+        val mediaButton = findViewById<Button>(R.id.Media_but)
+
+        searchButton.setOnClickListener {
+            val displayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+        mediaButton.setOnClickListener {
+            val displayIntent = Intent(this, MediatekActivity::class.java)
+            startActivity(displayIntent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/MessageActivity.kt
@@ -0,0 +1,6 @@
+package com.example.playlistmaker
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MessageActivity: AppCompatActivity() {
+}
