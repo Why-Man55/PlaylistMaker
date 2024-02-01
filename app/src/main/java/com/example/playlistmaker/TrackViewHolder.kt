97b36c3861e575a8042d07863168893f2871c5a3
Why-Man55 <Why2Man4@gmail.com>
2024-02-01 20:20:23 +0300

    спринт 10

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -2,7 +2,6 @@ package com.example.playlistmaker
 
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
@@ -16,11 +15,13 @@ class TrackViewHolder(parentView: ViewGroup) : RecyclerView.ViewHolder(LayoutInf
     private val artist = itemView.findViewById<TextView>(R.id.artist)
     private val time = itemView.findViewById<TextView>(R.id.time)
     private val url = itemView.findViewById<ImageView>(R.id.url)
+
+    private val radius: Float = 2 * itemView.resources.displayMetrics.density
     fun bind(model: Track) {
         Log.d("MY_TAG", "bind model=$model")
         name.text = model.trackName
         artist.text = model.trackArtist
         time.text = model.trackTime
-        Glide.with(itemView).load(model.trackAvatar).centerCrop().transform(RoundedCorners(2)).placeholder(R.drawable.empty_av).into(url)
+        Glide.with(itemView).load(model.trackAvatar).centerCrop().transform(RoundedCorners(radius.toInt())).placeholder(R.drawable.empty_av).into(url)
     }
 }
