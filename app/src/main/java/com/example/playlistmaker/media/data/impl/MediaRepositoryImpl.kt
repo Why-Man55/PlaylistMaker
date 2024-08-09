7e57225f38e5184e30083cf2ed45a455693072bd
kamil <why2man4@gmail.com>
2024-08-09 17:31:02 +0300

    Спринт 21 (Рабочая без косячков)

--- app/src/main/java/com/example/playlistmaker/media/data/impl/MediaRepositoryImpl.kt
+++ app/src/main/java/com/example/playlistmaker/media/data/impl/MediaRepositoryImpl.kt
@@ -5,7 +5,6 @@ import com.example.playlistmaker.media.data.converters.TrackConvertor
 import com.example.playlistmaker.media.data.db.AppDatabase
 import com.example.playlistmaker.search.domain.models.Track
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 
 class MediaRepositoryImpl(
