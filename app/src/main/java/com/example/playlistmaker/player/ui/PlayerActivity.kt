87ab1218d36a569075281743554985a08fe2dcea
kamil <why2man4@gmail.com>
2024-08-12 11:01:58 +0300

    Спринт 21 (Рабочая без косячков)

--- app/src/main/java/com/example/playlistmaker/player/ui/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/player/ui/PlayerActivity.kt
@@ -9,7 +9,6 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityPlayerBinding
-import com.example.playlistmaker.media.ui.FavoritesFragment
 import com.example.playlistmaker.player.presentation.PlayerViewModel
 import com.example.playlistmaker.search.domain.models.Track
 import kotlinx.coroutines.Dispatchers
