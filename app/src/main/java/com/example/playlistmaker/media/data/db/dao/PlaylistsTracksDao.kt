1a44970fc37e2d3e9065cec59c469c4a096b14c8
kamil <why2man4@gmail.com>
2024-09-11 16:16:21 +0300

    Спринт 23

--- app/src/main/java/com/example/playlistmaker/media/data/db/dao/PlaylistsTracksDao.kt
+++ app/src/main/java/com/example/playlistmaker/media/data/db/dao/PlaylistsTracksDao.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PlaylistsTracksDao {
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertPlaylistTrack(trackEntity: PlaylistsTrackEntity)
 
     @Query("Select * FROM playlist_track_table")
